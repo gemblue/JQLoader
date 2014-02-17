1a94ef368d46e52a0927d3b46e6ed891b78db68c
Ahmad Oriza <ahmadoriza@gmail.com>
2014-02-17 16:03:19 +0700

    Change core logic into standard Jq Plugin

--- js/looding.js
+++ js/looding.js
@@ -5,140 +5,110 @@
 |
 */
 
-var loader_content, lood_div, position;
-var window_width = $( window ).width();
-var window_height = $( window ).height();
-var mid =  window_width / 2;
-var lood_width = 220;
-
-/*
-Standart loading
-*/
-function standard_lood(config) 
-{	
-	/*
-	Black screen effect
-	*/
+(function ( $ ) {
+ 
+	var lood_div;
+	var window_width = $( window ).width();
+	var window_height = $( window ).height();
+	var mid =  window_width / 2;
+	var lood_width = 220;
 	
-	if (config.with_mask == true)
-	{
-		lood_mask = "<div id='looding_mask'></div>";
-		$('body').append(lood_mask);
-		
-		$('#looding_mask').css({'width':window_width,'height':window_height});
-		$('#looding_mask').fadeIn(500);
-	}
-		
-	/* 
-	Load type 
-	*/
+    $.fn.looding = function( options ) {
 	
-	loader_content = "<img src='img/" + config.type + ".gif' />"; 
-	 
-	/*
-	Create lood_div on body page
-	*/
+        var settings = $.extend({
+            theme: "standard",
+			mask: true,
+			action: "open"
+        }, options);
 	
-	lood_div = "<div id='looding_box'><div id='looding_content'>"+ loader_content + "</div></div>";
-	
-	$('#looding_box').remove();
-	$('body').append(lood_div);
-	
-	/*
-	Position
-	*/
-
-    switch (config.position) {
-		case "bottom-right":
-			$('#looding_box').css("bottom", "10px");
-			$('#looding_box').css("right", "10px");
-            break;
-		case "bottom-left":
-			$('#looding_box').css("bottom", "10px");
-			$('#looding_box').css("left", "10px");
-            break;
-		case "center-screen":
+		if (settings.theme == 'standard')
+		{
+			// Action
+			if (settings.action == 'close')
+			{
+				$('#looding_box').hide();
+				$('#looding_mask').hide();
+				return false;
+			}
+			
+			// Mask option
+			if (settings.mask == true)
+			{
+				// Set element
+				lood_mask = "<div id='looding_mask'></div>";
+				
+				// Append mask element
+				$('body').append(lood_mask);
+				
+				// Call mask
+				$('#looding_mask').css({'width':window_width,'height':window_height});
+				$('#looding_mask').fadeIn(500);
+			}
+			
+			// Set lood element
+			lood_div = "<div id='looding_box'><div id='looding_content'><img src='img/type-2.gif' /></div></div>";
+			$('body').append(lood_div);
+			
+			// Position
 			$('#looding_box').css("top", (window_height / 2));
 			$('#looding_box').css('left', mid - (lood_width / 2));
-            break;
-        case "top-center":
-			  $('#looding_box').css("top", "10px");
-            $('#looding_box').css('left', mid - (lood_width / 2));
-            break;
-        case "top-right":
-            $('#looding_box').css("top", "10px");
-			$('#looding_box').css("right", "10px");
-            break;
-		case "top-left":
-            $('#looding_box').css("top", "10px");
-			$('#looding_box').css("left", "10px");
-            break;
-        default:
-           
-            break;
-    }
-
-	$('#looding_box').show();
-}
-
-/*
-Nyankod.com style loading
-*/
-function nyan_lood(config)
-{
-	if (config.type == 'close')
-	{
-		$(".loading-nyan-bottom").animate(
-		{ opacity: 0 }, {
-			duration: 500,
-			complete: function() { 	
-			}
+					
+			// Show lood
+			$('#looding_box').show();
 		}
-		);
-		setTimeout(function() {
-			$(".loading-nyan-bottom").animate(
-				{ left: -window_width }, {
-					duration: 100,
-					easing: 'easeInQuad',
-					complete: function() { 
-						$(".loading-text-img").hide();
+		else if (settings.theme == 'nyankod')
+		{
+			if (settings.action == 'close')
+			{
+				$(".loading-nyan-bottom").animate(
+					{ opacity: 0 }, {
+						duration: 500,
+						complete: function() { 	
+						}
 					}
-				}
-			);
-		}, 500);
-		
-		return true;
-	}
-	
-	/*
-	Embed div
-	*/
-	lood_div = "<div class='loading-nyan-bottom'><div class='loading-text-img'>Loading <img src='img/loading_bottom.gif'/></div></div>";
-	$('body').append(lood_div);
-	
-	/*
-	Set style
-	*/
-	$(".loading-nyan-bottom").css("background", config.color);
-	$(".loading-nyan-bottom").css("opacity","1");
-	
-	/*
-	Start loading
-	*/
-	$(".loading-nyan-bottom").animate(
-		{ left: 0 }, {
-			duration: 1000,
-			easing: 'easeInQuad',
-			complete: function() { 
-				$(".loading-text-img").fadeIn("slow");
+				);
+				setTimeout(function() {
+					$(".loading-nyan-bottom").animate(
+						{ left: -window_width }, {
+							duration: 100,
+							easing: 'easeInQuad',
+							complete: function() { 
+								$(".loading-text-img").hide();
+							}
+						}
+					);
+				}, 500);
+				
+				return true;
+			}
+			else
+			{
+				// Set element
+				lood_div = "<div class='loading-nyan-bottom'><div class='loading-text-img'>Please wait..</div></div>";
+				$('body').append(lood_div);
+				
+				// Set style
+				$(".loading-nyan-bottom").css("background", "#444");
+				$(".loading-nyan-bottom").css("opacity","1");
+				
+				// Start
+				$(".loading-nyan-bottom").animate(
+					{ left: 0 }, {
+						duration: 1000,
+						easing: 'easeInQuad',
+						complete: function() { 
+							$(".loading-text-img").fadeIn("slow");
+						}
+					}
+				);	
+				
+				return false;	
 			}
 		}
-	);	
+		else
+		{
+			alert('Theme not found or defined');
+		}
+    };
 	
-	return false;
-}
-	
-$('#looding_box').live('click', function() {
-	$('#looding_box').hide();
-	$('#looding_mask').hide();
-});
+}( jQuery ));
